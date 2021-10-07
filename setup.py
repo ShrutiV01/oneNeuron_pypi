acb4d251b6044e5c7af258c7093d00276dcf5711
unknown <shrutiv01@gmail.com>
2021-10-08 00:21:58 +0530

    update readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "ShrutiV01"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="shrutiv01@gmail.com",
     description="Its a description of Perceptron",
